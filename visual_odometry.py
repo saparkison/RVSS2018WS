910126322ebf4aa2eacf67d9a4bdfbf8bba84aa2
James Coller <jcoller@Jamess-MacBook-Pro.local>
2018-02-08 18:27:52 +1100

    updated visual odometry

--- visual_odometry.py
+++ visual_odometry.py
@@ -2,6 +2,11 @@ import numpy as np
 import cv2
 import math
 
+# def visual_odometry( start_num, end_num )
+
+start_num = 19
+end_num = 274
+
 # params for ShiTomasi corner detection
 feature_params = dict( maxCorners = 100,
                        qualityLevel = 0.05,
@@ -18,18 +23,22 @@ lk_params = dict( winSize  = (15,15),
 color = np.random.randint(0,255,(100,3))
 
 # Open the file with read only permit
-folder = '/home/tariq/Dev/RVSS2018WS/test_log/'
-f = open(folder+'data_log.txt')
+folder = 'data_log/'
+f = open(folder+'data_out.txt')
 
-H = np.array([[  1.12375325e-01,  -4.86001720e-01,   8.77712777e+02],
-             [  -2.36834933e+00,   4.49234588e+00,   6.64034708e+02],
-             [   4.01039286e-04,   1.59276405e-02,   1.00000000e+00]],float)
+#H = np.array([[  1.12375325e-01,  -4.86001720e-01,   8.77712777e+02],
+#             [  -2.36834933e+00,   4.49234588e+00,   6.64034708e+02],
+#             [   4.01039286e-04,   1.59276405e-02,   1.00000000e+00]],float)
 
-# Read the first line 
+H = np.array([[ 1.22265638e+00, -9.02305713e-01, -3.24612144e+03],
+             [  1.27021068e+01, -2.43531947e+01, -1.38074249e+03],
+             [  4.74107545e-03, -8.12562362e-02,  1.00000000e+00]],float)
+
+# Read the first line
 line = f.readline()
 
 # Load an color image in grayscale
-img1 = cv2.imread(folder+line[0:9],0)
+img1 = cv2.imread(folder+line[0:8],0)
 # Warp source image to destination based on homography
 #img1 = cv2.warpPerspective(img1, H, (600,600))
 
@@ -38,25 +47,26 @@ mask = np.zeros_like(img1)
 
 ## If the file is not empty keep reading line one at a time
 ## till the file is empty
-while line:
+for x in range(start_num, end_num): #start image, end image
     # Get corners
     p0 = cv2.goodFeaturesToTrack(img1, mask = None, **feature_params)
 
     # Load an color image in grayscale
     line = f.readline()
-    img2 = cv2.imread(folder+line[0:9],0)
-   
+    img2 = cv2.imread(folder+line[0:8],0)
+
     # Warp source image to destination based on homography
     #img2 = cv2.warpPerspective(img2, H, (600,600))
-   
+
     # Track corners
+    print "Name: %s" % (folder+line[0:8])
     p1, st, err = cv2.calcOpticalFlowPyrLK(img1, img2, p0, None, **lk_params)
-    
+
     # Select good points
     st = st.reshape(st.shape[0])
     good_old = p0[st==1]
     good_new = p1[st==1]
-    
+
     # Prespective Transformation
     p0 = cv2.perspectiveTransform(good_old, H)
     p1 = cv2.perspectiveTransform(good_new, H)
@@ -86,8 +96,12 @@ while line:
         frame = cv2.circle(frame,(a,b),5,color[i].tolist(),-1)
     img = cv2.add(frame,mask)
     cv2.imshow('frame',img)
-    cv2.waitKey(0)
+    cv2.waitKey(0) & 0xFF
     mask = np.zeros_like(img1)
+
+    print "I am here."
 
 cv2.destroyAllWindows()
 f.close()
+
+print "Now I am here!"
